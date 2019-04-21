cf336ba861436076c0a30a6c3ee60db05669d39e
Lorenzo Moreti <lorenzomoreti@yahoo.com>
2019-04-21 02:38:11 -0300

    comecando a colocar pecas no tabuleiro

---
+++ xadrez-console/Xadrez/Torre.cs
@@ -0,0 +1,16 @@
+﻿using tabuleiro;
+
+namespace Xadrez
+{
+    class Torre : Peca
+    {
+        public Torre(Cor cor, Tabuleiro tab) : base(cor, tab)
+        {
+        }
+
+        public override string ToString()
+        {
+            return "T";
+        }
+    }
+}
